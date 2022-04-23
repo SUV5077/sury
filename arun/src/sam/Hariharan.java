0a1a0ea699a58b55d02b1fb3cd7d97a3b3979d6d
surya prakash V <surya prakash V@DESKTOP-GVOM045>
2022-04-23 13:40:54 +0530

    project1

---
+++ arun/src/sam/Hariharan.java
@@ -0,0 +1,17 @@
+package sam;
+
+public class Hariharan {
+	public static void main(String args[]) { 
+	
+	String name1 = "suryprakash";
+			
+		System.out.print("string length is:"+name1.length() );
+	
+	
+	}
+
+	
+}
+
+	
+
